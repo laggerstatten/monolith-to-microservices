b8031afa110ef60915d9675ccee3fbb4b625eb7b
Eric Schall <61393763+laggerstatten@users.noreply.github.com>
2024-01-25 10:17:31 -0600

    url change

--- set_env.sh
+++ set_env.sh
@@ -8,4 +8,4 @@ export AWS_BUCKET=my-laggerstatten-1-bucket
 export AWS_REGION=us-east-1
 export AWS_PROFILE=udagram-admin
 export JWT_SECRET=testing
-export URL=http://localhost:8100
+export URL=http://a2be403475fe142e688701bf612a4af1-1094786878.us-east-1.elb.amazonaws.com:8100
